6360dece6893704576008e40d264dbf69b06a3e2
Yaoxing <yaoxing.zhang@gmail.com>
2018-08-11 18:08:14 +0800

    + generate orders and orderlines. each order has 250~350 orderlines

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
 var express = require('express');
+var faker = require('faker');
+var config = require('config');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  let pool = req.app.get('pool');
+  pool.getConnection((err, conn) => {
+    conn.execute(`SELECT TAPDATA.TAPSEQ.nextval FROM DUAL`, {}, {
+      autoCommit: true
+    }, (err, result) => {
+      if (err) {
+        res.status(500);
+        res.json({
+          err: err
+        });
+        return;
+      }
+
+      let orderId = result.rows[0][0];
+      conn.execute(`INSERT INTO TAPDATA.ORDERS VALUES (:seq, :street, :city, :state, :country, :zip, :phone, :name, :userid)`, [
+        orderId,
+        faker.address.streetAddress(),
+        faker.address.city(),
+        faker.address.state(),
+        faker.address.country(),
+        faker.address.zipCode(),
+        faker.phone.phoneNumber(),
+        `${faker.name.firstName()} ${faker.name.lastName()}`,
+        faker.random.number(config.app.totalUsers)
+      ], {
+        autoCommit: true
+      }, (err, result) => {
+        if (err) {
+          res.status(500);
+          res.json({
+            err: err
+          });
+          return;
+        }
+        let sql = `INSERT INTO TAPDATA.ORDER_LINES VALUES(TAPDATA.OLSEQ.nextval, :orderid, :product, :sku, :qty, :price)`;
+        let totalLines = 250 + faker.random.number(100);
+        let binds = []
+        for(let i = 0; i < totalLines; i++) {
+          binds.push({
+            orderid: orderId,
+            product: faker.commerce.productName(),
+            sku: faker.random.number(10000).toString(),
+            qty: faker.random.number(100),
+            price: faker.commerce.price()
+          });
+        }
+        conn.executeMany(sql, binds, {
+          autoCommit: true
+        }, (err, result) => {
+          if (err) {
+            res.status(500);
+            res.json({
+              err: err
+            });
+            return;
+          }
+
+          conn.close((err => {
+            res.json({
+              ok: 1
+            });
+          }));
+        })
+      });
+    });
+  });
 });
 
 module.exports = router;
